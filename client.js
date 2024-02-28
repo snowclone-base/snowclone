e35fce44dfa10b4d470e44ae5e00e99b9da4983d
Sarah Voshall <sarahvoshall@gmail.com>
2024-02-28 00:55:15 -0800

    added db listener client to display notification payloads -- must send payload as JSON

---
+++ client.js
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import createSubscriber from "pg-listen";
+
+const subscriber = createSubscriber({
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+});
+
+subscriber.notifications.on("channel", (payload) => {
+  // Payload as passed to subscriber.notify() -> must pass payload as JSON
+  console.log("Received notification in 'channel':", payload);
+});
+
+subscriber.events.on("error", (error) => {
+  console.error("Fatal database connection error:", error);
+  process.exit(1);
+});
+
+process.on("exit", () => {
+  subscriber.close();
+});
+
+await subscriber.connect();
+await subscriber.listenTo("channel");
