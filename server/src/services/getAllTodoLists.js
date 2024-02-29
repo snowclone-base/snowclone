85f04a2234d4ce5822fc727451c20ee3d8b44a4e
Tim Williams <timw636@gmail.com>
2024-02-28 21:12:08 -0800

    Revert "added API for createTodo and createTodoList"
    This reverts commit 4d86bbba9276a982870d272d12ee1dd9b8297e43.

--- server/src/services/getAllTodoLists.js
+++
@@ -1,8 +0,0 @@
-import { dbQuery } from "./db-query.js";
-
-export const getAllTodoLists = async () => {
-  const SQL = "SELECT * FROM todolists;";
-
-  const result = await dbQuery(SQL);
-  return result.rows;
-};
