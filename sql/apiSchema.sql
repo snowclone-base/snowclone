58ade1a0851ea8c0173fb1547c572f4d4df54423
Tim Williams <timw636@gmail.com>
2024-03-17 21:00:37 -0700

    fixed bug postgrest bug introduced by deleting web_anon user in db

--- sql/apiSchema.sql
+++ sql/apiSchema.sql
@@ -2,15 +2,40 @@ create schema api;
 
 create role authenticator noinherit login password 'mysecretpassword';
 
--- creating placeholder account for admin, with full access
+-- Create web anon role and grant usage on schema api, and then grant default read privileges
+-- on all tables in schema api to allow viewing openAPI spec from browser at localhost:3000
+-- without a token.
+create role web_anon nologin;
+grant usage on schema api to web_anon;
+grant web_anon to authenticator;
+
+alter default privileges in schema api
+grant select on tables to web_anon;
+
+
+-- creating placeholder account for admin, dev_admin ,  with full access
 -- to api schema. making sure that JWT setup is working. 
 create role dev_admin nologin;
+grant usage on schema api to dev_admin;
 grant dev_admin to authenticator;
 
-grant usage on schema api to dev_admin;
-grant all on all tables in schema api to dev_admin;
-grant all on all sequences in schema api to dev_admin;
+-- These ALTER DEFAULT PRIVILEGES do what I thought the commented out grant
+-- options did below-- grant all access in schema api EVEN FOR OBJECTS THAT
+-- AREN'T YET CREATED. Grant only works on previously created objects.
+alter default privileges in schema api grant all on TABLES TO dev_admin;
+alter default privileges in schema api grant all on SEQUENCES TO dev_admin;
+alter default privileges in schema api grant all on FUNCTIONS TO dev_admin;
+-- grant all on all tables in schema api to dev_admin;
+-- grant all on all sequences in schema api to dev_admin;
 
+
+
+-- Need to do:
+-- look more into security. locking down db to only our dev_admin and 
+-- web_anon users.
+
+
+--  Below is instructions for initializing schema in postgres docker image
 
 /* If you would like to do additional initialization in an image derived from this one, 
 add one or more *.sql, *.sql.gz, or *.sh scripts under /docker-entrypoint-initdb.d 
