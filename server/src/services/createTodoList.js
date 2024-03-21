3283e3d466d1adc4cba725c0e25a2c24ab683d18
Sarah Voshall <sarahvoshall@gmail.com>
2024-03-21 15:06:34 -0700

    updated and renamed server to schema-server, deleted old CRUD routes for todolist API

--- server/src/services/createTodoList.js
+++
@@ -1,8 +0,0 @@
-import { dbQuery } from "./db-query.js";
-
-export const createTodoList = async (title, username) => {
-  const SQL = "INSERT INTO todolists(title, username)" + "VALUES($1, $2);";
-
-  const result = await dbQuery(SQL, title, username);
-  return result.rows;
-};
