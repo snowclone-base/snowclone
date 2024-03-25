763e54f6f19ed62ff3bc1a494993c070eef48cd6
Sarah Voshall <sarahvoshall@gmail.com>
2024-03-25 10:57:07 -0700

    cleanup

--- schema-server/src/index.js
+++ schema-server/src/index.js
@@ -2,45 +2,15 @@ import "dotenv/config.js";
 import cors from "cors";
 import express from "express";
 import fs from "fs";
-import multer from "multer";
-import path from "path";
+
 import verifyToken from "./middleware/verifyToken.js";
 import { upload, storagePath } from "./middleware/uploadFile.js";
 
 import { dbQuery } from "./services/db-query.js";
 
-// utils to create path to file
-// import { fileURLToPath } from "url";
-// import { dirname } from "path";
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-// const storagePath = path.join(__dirname, "tmp", "my-uploads");
-
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-// tell multer where to store the file
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, storagePath);
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// app.post("/schema", verifyToken, upload.single("file"), async (req, res) => {
-//   // example curl request for uploading a file, @test.sql represent the local path
-//   // curl -F 'file=@test.sql' http://localhost:5175/schema
-
-//   var sql = fs.readFileSync(`${storagePath}/${req.file.originalname}`, "utf8");
-
-//   await dbQuery(sql);
-//   res.status(201).send({ message: "Schema update successful!" });
-// });
 
 app.post("/schema", verifyToken, upload.single("file"), async (req, res) => {
   try {
@@ -67,7 +37,7 @@ app.post("/schema", verifyToken, upload.single("file"), async (req, res) => {
 
 // healthcheck path
 app.get("/V1/api", (req, res) => {
-  res.send("You made it!");
+  res.status(200).send({ message: "Schema import API server is healthy." });
 });
 
 app.listen(5175, () => {
