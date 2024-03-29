3068e379452da4e025d072cad1511be57e205639
Sarah Voshall <sarahvoshall@gmail.com>
2024-03-28 18:40:29 -0700

    update schema server to receive env variables as parameters

--- schema-server/src/services/db-query.js
+++ schema-server/src/services/db-query.js
@@ -4,8 +4,14 @@ import pkg from "pg";
 const { Client } = pkg;
 import "dotenv/config";
 
+const { PG_USER, PG_PASSWORD, PG_HOST, PG_PORT, PG_DATABASE } = process.env;
+
 const CONNECTION = {
-  connectionString: process.env.DATABASE_URL,
+  user: PG_USER,
+  password: PG_PASSWORD,
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
 };
 
 export const dbQuery = async (statement, ...parameters) => {
